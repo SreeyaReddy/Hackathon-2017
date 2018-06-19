3d06775359f97c83372926b4afe22f7b58aa78ce
sreelekha23 <sreelekha.kreddy@gmail.com>
2018-06-19 09:08:07 -0500

    app

---
+++ Source/CityGuide/app/src/main/java/domain/muktevi/cityguide/TrendsBO.java
@@ -0,0 +1,16 @@
+package domain.muktevi.cityguide;
+
+import java.util.List;
+
+
+public class TrendsBO {
+    List<String> trends;
+
+    public List<String> getTrends() {
+        return trends;
+    }
+
+    public void setTrends(List<String> trends) {
+        this.trends = trends;
+    }
+}
